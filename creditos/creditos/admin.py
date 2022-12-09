d093d1bd3409cfe4ad921d0d6a0511617977f135
marianadiaz179 <78369609+marianadiaz179@users.noreply.github.com>
2022-12-08 20:55:55 -0500

    nnn

--- creditos/creditos/admin.py
+++ creditos/creditos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Measurement
+from . models import Credito
 
 # Register your models here.
-admin.site.register(Measurement)
+admin.site.register(Credito)
--- creditos/monitoring/settings.py
+++ creditos/monitoring/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'measurements',
 ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
